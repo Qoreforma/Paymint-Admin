637f6056da2ac3f422a1cc0064bac05e808b3eb4
Benjamin <benferanmiopafunso@gmail.com>
2026-08-25 01:43:48 +0100

    fix react-query import

--- src/api/treasury.js
+++ src/api/treasury.js
@@ -1,4 +1,4 @@
-import { useQuery, useMutation, useQueryClient } from "react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import BACKEND_URLS from "./urls";
 import requests from "./requests";
 
